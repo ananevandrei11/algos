26273107b1495cd862358a9d6023ba1001bb8062
ananevandrei11 <ananevandrei11@gmail.com>
2026-06-04 16:34:01 +0200

    summaryRanges

--- src/summaryRanges/summaryRanges.ts
+++ src/summaryRanges/summaryRanges.ts
@@ -28,5 +28,3 @@ export function summaryRanges(nums: number[]): string[] {
   }
   return res;
 }
-summaryRanges([0, 1, 2, 4, 5, 7]); // ['0->2', '4->5', '7']
-// summaryRanges([0, 2, 3, 4, 6, 8, 9]); // ['0', '2->4', '6', '8->9']
