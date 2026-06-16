bf7121ae7b9207a4dafaf24595f4295a2540c104
ananevandrei11 <ananevandrei11@gmail.com>
2026-06-16 11:03:35 +0200

    spiralOrder

--- src/spiralOrder/spiralOrder.ts
+++ src/spiralOrder/spiralOrder.ts
@@ -31,10 +31,3 @@ export function spiralOrder(matrix: number[][]): number[] {
   }
   return res;
 }
-
-spiralOrder([
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-]);
-// .toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
